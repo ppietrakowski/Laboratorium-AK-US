912dfbad2af58ed1441cd0ea7ed1c894fc40ce36
Pawel Pietrakowski <pawelpietrakowski22012001@gmail.com>
2021-11-09 20:14:22 +0100

    Added some comments

--- program.asm
+++ program.asm
@@ -1,5 +1,7 @@
 
 	ORG 800H  
+
+    ; Wyswietlenie menu
 	LXI H,WPR_OPERAND  
 	RST 3
     RST 5
@@ -7,9 +9,11 @@
     LXI H,SYM_NOWALINIA
     RST 3
 
+    ; przekopiowanie wartosci do rejestrow pomocniczych
     MOV B,D
     MOV C,E     
 
+; petla wykonujaca sie dopoki nie znaleziono odpowiedniego znaku
 ZNAK_DZIALANIA
     LXI H,WPR_DZIALANIE
     RST 3
@@ -17,14 +21,17 @@ ZNAK_DZIALANIA
 
     LXI H,SYM_NOWALINIA
     RST 3
-    CPI '!'
 
+    ; wyszukiwanie wprowadzonego operatora
+
+    CPI '!'
     CZ NEGACJA
     JZ WYNIK
     
     CPI '+'
     JZ DODAWANIE
 
+    ; nie znaleziono odpowiedniego operatora->popros o wprowadzenie ponownie
     JMP ZNAK_DZIALANIA
 
 WYNIK
@@ -34,44 +41,57 @@ WYNIK
     LXI H,WYN_DZIALANIA
     RST 3
 
+    ; skok pod warunkiem C=1
     CC WYNIK_Przen
 
     MOV A,B
     RST 4
+
     MOV A,C
     RST 4
 	HLT  
 
+; wczytuje liczbe i zapisuje w rejestrach DE
 WCZYTAJ_LICZBE
     LXI H,WPR_OPERAND2
     RST 5
     RET
 
+; neguje liczbe znajdujaca sie w rejestrach DE
 NEGACJA
     MOV A,B
+    ; negacja bitow akumulatora
     CMA
     MOV B,A
+
     MOV A,C
     CMA
     MOV C,A
+    
     RET
 
+; Dodaje DE do BC i zapisuje wynik w BC(kolejnosc starszy bajt-mlodszy bajt)
 DODAWANIE
     CALL WCZYTAJ_LICZBE
+    ; pierwsze dodawanie mozna wykonac bez przeniesienia
     MOV A,C
     ADD E
     MOV C,A
 
+    ; drugie juz trzeba, gdyz moglo na 8-osmym bicie nastapic przeniesienie
     MOV A,B
     ADC D
     MOV B,A
     JMP Wynik
     
+; wyswietla jedynke na znak, ze nastapilo przeniesienie
 WYNIK_Przen
     MVI A,'1'
     RST 1
     RET
 
+
+; "assety"
 SYM_NOWALINIA    DB 10,13,'@'
 WPR_OPERAND 	 DB 'Wprowadz operand',10,13,'@' 
 WPR_DZIALANIE 	 DB 'Wprowadz znak dzialania(+ - !)',10,13,'@' 
